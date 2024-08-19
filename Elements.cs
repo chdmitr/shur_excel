7a8aa6e5c4208873a34a34068f17e5ca3473f30b
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 09:08:02 +0000

    add row reading

--- Elements.cs
+++ Elements.cs
@@ -2,19 +2,26 @@ using System.Collections.Generic;
 
 namespace shur_excel;
 
-class Element
+public class Element
 {
     public int NumFrom { get; }
     public int NumTo { get; }
     public double LowerBound { get; }
     public double UpperBound { get; }
-
-    public Element(int numFrom, int numTo, double lowerBound, double upperBound)
+    public double Value { get; set; }
+    
+    public Element(int numFrom, int numTo, double lowerBound, double upperBound, double value = 0.0)
     {
         NumFrom = numFrom;
         NumTo = numTo;
         LowerBound = lowerBound;
         UpperBound = upperBound;
+        Value = value;
+    }
+
+    public Element Clone()
+    {
+        return new Element(NumFrom, NumTo, LowerBound, UpperBound, Value);
     }
 }
 
---
+++ Row.cs
@@ -0,0 +1,24 @@
+using System.Globalization;
+
+namespace shur_excel;
+
+public class Row
+{   
+    public Dictionary<string, Element> Elements { get; private set; }
+    public DateTime DateTime { get; }
+
+    public Row(string line)
+    {
+        Elements = new Dictionary<string, Element>();
+        var columns = line.Split('|').Select(c => c.Trim()).ToArray();
+        DateTime = DateTime.ParseExact($"{columns[0]} {columns[1]}", "dd.MM.yyyy hh:mm:ss", CultureInfo.InvariantCulture);
+        for (int i = 3; i < columns.Length; i++)
+        {
+            var itemPair = Program.elements.FirstOrDefault(c => c.Value.NumFrom == i);
+            if (itemPair.Key == null) { continue; }
+            Element element = itemPair.Value.Clone();
+            element.Value = double.Parse(columns[i]);            
+            Elements.Add(itemPair.Key, element);
+        }
+    }
+}
