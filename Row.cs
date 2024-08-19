9e8b8dfc1097925eaac97e207e2616bff7ec3d6a
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 18:51:32 +0000

    sugar fix

--- Row.cs
+++ Row.cs
@@ -9,14 +9,14 @@ public class Row
 
     public Row(string line)
     {
-        RowElements = new Dictionary<string, Element>();
+        RowElements = [];
         var columns = line.Split('|').Select(c => c.Trim()).ToArray();
         DateTime = DateTime.ParseExact($"{columns[0]} {columns[1]}", "dd.MM.yyyy hh:mm:ss", CultureInfo.InvariantCulture);
         for (int i = 3; i < columns.Length; i++)
         {
             var itemPair = Program.configElements.FirstOrDefault(c => c.Value.NumFrom == i);
             if (itemPair.Key == null) { continue; }
-            Element element = new Element(itemPair.Value, double.Parse(columns[i]));
+            var element = new Element(itemPair.Value, double.Parse(columns[i]));
             RowElements.Add(itemPair.Key, element);
         }
     }
