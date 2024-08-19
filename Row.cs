b8c10fca667b76b478416b12c69f91f2bba929fc
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 18:55:59 +0000

    rename var

--- Row.cs
+++ Row.cs
@@ -4,12 +4,12 @@ namespace shur_excel;
 
 public class Row
 {
-    public Dictionary<string, Element> RowElements { get; private set; }
+    public Dictionary<string, Element> Elements { get; private set; }
     public DateTime DateTime { get; }
 
     public Row(string line)
     {
-        RowElements = [];
+        Elements = [];
         var columns = line.Split('|').Select(c => c.Trim()).ToArray();
         DateTime = DateTime.ParseExact($"{columns[0]} {columns[1]}", "dd.MM.yyyy hh:mm:ss", CultureInfo.InvariantCulture);
         for (int i = 3; i < columns.Length; i++)
@@ -17,7 +17,7 @@ public class Row
             var itemPair = Program.configElements.FirstOrDefault(c => c.Value.NumFrom == i);
             if (itemPair.Key == null) { continue; }
             var element = new Element(itemPair.Value, double.Parse(columns[i]));
-            RowElements.Add(itemPair.Key, element);
+            Elements.Add(itemPair.Key, element);
         }
     }
 }
