de837c2ebae7684258ceafafce38e4abdbe8cf17
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 18:13:14 +0000

    class to record refactoring

--- Elements.cs
+++ Elements.cs
@@ -1,27 +1,26 @@
-using System.Collections.Generic;
-
 namespace shur_excel;
 
-public class Element
+public record BasicElement
 {
     public int NumFrom { get; }
     public int NumTo { get; }
     public double LowerBound { get; }
     public double UpperBound { get; }
-    public double Value { get; set; }
 
-    public Element(int numFrom, int numTo, double lowerBound, double upperBound, double value = 0.0)
+    public BasicElement(int numFrom, int numTo, double lowerBound, double upperBound)
     {
         NumFrom = numFrom;
         NumTo = numTo;
         LowerBound = lowerBound;
         UpperBound = upperBound;
-        Value = value;
-    }
-
-    public Element Clone()
-    {
-        return new Element(NumFrom, NumTo, LowerBound, UpperBound, Value);
     }
 }
 
+public record Element : BasicElement
+{
+    public double Value { get; init; }
+    public Element(BasicElement original, double value) : base(original)
+    {
+        Value = value;
+    }
+}
--- Row.cs
+++ Row.cs
@@ -4,21 +4,20 @@ namespace shur_excel;
 
 public class Row
 {
-    public Dictionary<string, Element> Elements { get; private set; }
+    public Dictionary<string, Element> RowElements { get; private set; }
     public DateTime DateTime { get; }
 
     public Row(string line)
     {
-        Elements = new Dictionary<string, Element>();
+        RowElements = new Dictionary<string, Element>();
         var columns = line.Split('|').Select(c => c.Trim()).ToArray();
         DateTime = DateTime.ParseExact($"{columns[0]} {columns[1]}", "dd.MM.yyyy hh:mm:ss", CultureInfo.InvariantCulture);
         for (int i = 3; i < columns.Length; i++)
         {
             var itemPair = Program.configElements.FirstOrDefault(c => c.Value.NumFrom == i);
             if (itemPair.Key == null) { continue; }
-            Element element = itemPair.Value.Clone();
-            element.Value = double.Parse(columns[i]);
-            Elements.Add(itemPair.Key, element);
+            Element element = new Element(itemPair.Value, double.Parse(columns[i]));
+            RowElements.Add(itemPair.Key, element);
         }
     }
 }
