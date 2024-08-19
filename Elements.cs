c9def91361c56f8067a58dc422f6ff55453de6ee
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 18:24:05 +0000

    record refactoring

--- Elements.cs
+++ Elements.cs
@@ -1,21 +1,6 @@
 namespace shur_excel;
 
-public record BasicElement
-{
-    public int NumFrom { get; }
-    public int NumTo { get; }
-    public double LowerBound { get; }
-    public double UpperBound { get; }
-
-    public BasicElement(int numFrom, int numTo, double lowerBound, double upperBound)
-    {
-        NumFrom = numFrom;
-        NumTo = numTo;
-        LowerBound = lowerBound;
-        UpperBound = upperBound;
-    }
-}
-
+public record BasicElement(int NumFrom, int NumTo, double LowerBound, double UpperBound);
 public record Element : BasicElement
 {
     public double Value { get; init; }
