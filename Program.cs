be13337407738592c70f1ab88fdc3df9c735d906
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2024-08-19 09:10:00 +0000

    refactoring var

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace shur_excel;
 
 class Program
 {
-    public static Dictionary<string, Element> elements { get; set;}
+    public static Dictionary<string, Element> configElements { get; set;}
 
     static void Main(string[] args)
     {
@@ -13,7 +13,7 @@ class Program
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .Build();
 
-        elements = config.GetRequiredSection("elements").Get<Dictionary<string, Element>>();
+        configElements = config.GetRequiredSection("elements").Get<Dictionary<string, Element>>();
         
         var line = "01.02.2024 | 09:22:30 | 1       |   99.983888 |  0.001272 |   0.001178 |  0.002204 |  0.001293 |  0.003327 |   0.000283 |  0.000643 |   0.000410 |   0.000473 |  0.000343 |  0.000547 |  0.002915 |  0.000296 |   0.000000 |   0.000000 |   0.000231 |  0.000137 |   0.000471 |  0.000090";
 
--- Row.cs
+++ Row.cs
@@ -14,7 +14,7 @@ public class Row
         DateTime = DateTime.ParseExact($"{columns[0]} {columns[1]}", "dd.MM.yyyy hh:mm:ss", CultureInfo.InvariantCulture);
         for (int i = 3; i < columns.Length; i++)
         {
-            var itemPair = Program.elements.FirstOrDefault(c => c.Value.NumFrom == i);
+            var itemPair = Program.configElements.FirstOrDefault(c => c.Value.NumFrom == i);
             if (itemPair.Key == null) { continue; }
             Element element = itemPair.Value.Clone();
             element.Value = double.Parse(columns[i]);            
